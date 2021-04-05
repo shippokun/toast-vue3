7552100420c65124b5823a36467e68f6e08ff9df
shippokun <okashi0tabetai@gmail.com>
2021-04-06 01:37:38 +0900

    feat(pages/todo): createの中身を追加

--- src/pages/todo/todo-create/components/todo-create/todo-create.component.vue
+++ src/pages/todo/todo-create/components/todo-create/todo-create.component.vue
@@ -1,5 +1,43 @@
 <template>
-  <div />
+  <nav class="panel is-info">
+    <p class="panel-heading">TodoCreate</p>
+    <div class="px-5 pt-5">
+      <div class="field">
+        <label class="label">Title</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="Title input" />
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Content</label>
+        <div class="control">
+          <textarea class="textarea" placeholder="Textarea" />
+        </div>
+      </div>
+      <div class="field">
+        <div class="control">
+          <label class="checkbox">
+            <input type="checkbox" />
+            Already Complated?
+          </label>
+        </div>
+      </div>
+    </div>
+    <div class="panel-block is-justify-content-flex-end p-5">
+      <div class="field is-grouped">
+        <p class="control">
+          <button class="button is-outlined is-info" @click="resetEvent">
+            Create
+          </button>
+        </p>
+        <p class="control">
+          <router-link :to="{ path: '/todos' }">
+            <a class="button">Back</a>
+          </router-link>
+        </p>
+      </div>
+    </div>
+  </nav>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
