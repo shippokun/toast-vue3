f0d001875aa89e0de1b66f6776b0ff40688d26dc
shippokun <okashi0tabetai@gmail.com>
2021-04-06 20:34:26 +0900

    chore(pages/todo): 簡易的にフィードバックするためにtoast導入

--- src/pages/todo/todo-create/containers/todo-create/todo-create.container.vue
+++ src/pages/todo/todo-create/containers/todo-create/todo-create.container.vue
@@ -1,15 +1,26 @@
 <template>
-  <todo-create-component />
+  <todo-create-component :onCreate="onCreate" />
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 
+import { TodoCreateDto } from "@/models";
 import { TodoCreateComponent } from "../../components";
+import { useToast } from "vue-toastification";
 
 export default defineComponent({
   name: "TodoCreateContainer",
   components: {
     TodoCreateComponent,
+  },
+  setup() {
+    const toast = useToast();
+    const onCreate = (todo: TodoCreateDto) => {
+      const text = Object.entries(todo).join(" ");
+      toast.success(text);
+    };
+
+    return { onCreate };
   },
 });
 </script>
