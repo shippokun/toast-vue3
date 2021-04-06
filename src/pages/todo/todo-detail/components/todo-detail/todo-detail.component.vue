9667926481040d9b97ca47c1e9ed50f70f7421df
shippokun <okashi0tabetai@gmail.com>
2021-04-06 21:34:20 +0900

    feat(pages/todo): detailのデザイン修正

--- src/pages/todo/todo-detail/components/todo-detail/todo-detail.component.vue
+++ src/pages/todo/todo-detail/components/todo-detail/todo-detail.component.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>{{ todo }}</div>
+  <nav class="panel is-info">
+    <p class="panel-heading">TodoDetail</p>
+    <div class="px-3 pt-3">
+      <div class="field">
+        <label class="label">Title</label>
+        <div class="control">
+          <p>
+            {{ todo.title }}
+          </p>
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Context</label>
+        <div class="control">
+          <p>{{ todo.context }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="panel-block is-justify-content-flex-end p-3">
+      <div class="field is-grouped">
+        <p class="control">
+          <router-link :to="{ path: '/todos/edit' }">
+            <a class="button is-outlined is-info">Edit</a>
+          </router-link>
+        </p>
+        <p class="control">
+          <router-link :to="{ path: '/todos' }">
+            <a class="button">Back</a>
+          </router-link>
+        </p>
+      </div>
+    </div>
+  </nav>
 </template>
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
