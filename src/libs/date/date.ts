b1473396bfaecbd2ae5597f91e023b4784ed5a33
shippokun <okashi0tabetai@gmail.com>
2021-04-08 21:19:37 +0900

    feat(libs): formatDateのinputにstringを追加

--- src/libs/date/date.ts
+++ src/libs/date/date.ts
@@ -1,9 +1,13 @@
-import { format } from "date-fns";
+import { format, parseISO } from "date-fns";
 
 export const formatDate = (
-  value: Date | undefined,
+  value: Date | string | undefined,
   formatType = "yyyy-MM-dd HH:mm:ss"
 ): string => {
   if (value === undefined) return "";
+  if (typeof value === "string") {
+    const result = parseISO(value);
+    return format(result, formatType);
+  }
   return format(value, formatType);
 };
