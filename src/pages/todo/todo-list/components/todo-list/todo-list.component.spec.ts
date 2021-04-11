073d66a23a1f7020b0c90d88b6d9da3697f1020b
shippokun <okashi0tabetai@gmail.com>
2021-04-11 22:36:01 +0900

    test(todo-list): add todo-list component snapshot

---
+++ src/pages/todo/todo-list/components/todo-list/todo-list.component.spec.ts
@@ -0,0 +1,38 @@
+import { mount } from "@vue/test-utils";
+import { createTodosMock } from "@/models/testing";
+import { TodoListComponent } from "./";
+import { router } from "@/router";
+
+describe("todo-list.component.vue", () => {
+  describe("renders", () => {
+    it("default render", () => {
+      const wrapper = mount(TodoListComponent, {
+        props: {
+          reset: jest.fn(),
+        },
+        global: {
+          plugins: [router],
+        },
+      });
+
+      expect(wrapper.html()).toMatchSnapshot();
+    });
+    it("props todos", () => {
+      const todos = createTodosMock();
+      const mockFn = jest.fn();
+      const wrapper = mount(TodoListComponent, {
+        props: {
+          todos,
+          reset: mockFn,
+        },
+        global: {
+          plugins: [router],
+        },
+      });
+
+      expect(wrapper.html()).toMatchSnapshot();
+      wrapper.find("button").trigger("click");
+      expect(mockFn).toHaveBeenCalled();
+    });
+  });
+});
