50acca21f7410b8ed877ea79b8cabb6f829c31af
shippokun <okashi0tabetai@gmail.com>
2021-04-11 21:17:26 +0900

    chore(jest.config.js): update

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  testMatch: ["**/+(*.)+(spec|test).+(ts|js)?(x)"],
   transform: {
     "^.+\\.vue$": "vue-jest",
   },
