d727b969e1cd97b4454f78fe107a8a0a7632577d
shippokun <okashi0tabetai@gmail.com>
2021-04-23 00:59:29 +0900

    chore(App): remove scss

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,3 @@
 <template>
   <router-view />
 </template>
-
-<style lang="scss" scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
