0bb07caa31483750f37b0c7d9dbf184f1d45211d
shippokun <okashi0tabetai@gmail.com>
2021-05-15 14:20:47 +0900

    feat(component/vscode): vscodeを表示するモーダルの追加

---
+++ src/components/vscode/vscode.component.tsx
@@ -0,0 +1,34 @@
+import { defineComponent, PropType, Ref, ref } from "vue";
+import { ModalComponent } from "@/components";
+import styled from "vue3-styled-components";
+
+type Props = {
+  isShow?: Ref<boolean>;
+};
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 80vh;
+`;
+
+export const VSCode = defineComponent({
+  name: "VSCodeComponent",
+  components: { ModalComponent, Iframe },
+  props: {
+    isShow: {
+      type: Object as PropType<Props["isShow"]>,
+      default: ref(false),
+    },
+  },
+  setup(props: Props) {
+    return () => (
+      <ModalComponent isShow={props.isShow}>
+        <Iframe
+          src="https://github1s.com/shippokun/toast-vue3/blob/main/README.md"
+          frameborder="0"
+          title="VSCode"
+        />
+      </ModalComponent>
+    );
+  },
+});
