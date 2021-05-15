ad6b2431ec0f69aab57c6abb3519d6e9a01470b4
shippokun <okashi0tabetai@gmail.com>
2021-05-15 14:21:13 +0900

    feat(layout/todo): vscode modalを表示するリンクを追加

--- src/layouts/todo/todo.layout.tsx
+++ src/layouts/todo/todo.layout.tsx
@@ -1,17 +1,29 @@
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { RouterView } from "vue-router";
+import { VSCode } from "@/components";
 
 export const TodoLayout = defineComponent({
   name: "TodoLayout",
+  components: { VSCode },
   setup() {
+    const isVSCodeShow = ref(false);
+    const onVSCodeShow = () => {
+      isVSCodeShow.value = !isVSCodeShow.value;
+    };
+
+    <button onClick={() => onVSCodeShow()} />;
     return () => (
       <div class="container is-max-desktop">
         <section class="hero">
-          <div class="hero-body">
-            <h1 class="title has-text-centered">Todo-Vue</h1>
+          <div class="hero-body  has-text-centered">
+            <h1 class="title">Todo-Vue</h1>
+            <a onClick={() => onVSCodeShow()}>
+              Show GithubCode for VSCodeModal
+            </a>
           </div>
         </section>
         <RouterView />
+        <VSCode isShow={isVSCodeShow} />
       </div>
     );
   },
